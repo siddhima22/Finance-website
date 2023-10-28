adee0584eda20661aa5a1ac2f333ef60d91258ea
Ritojnan <ritojnanm@gmail.com>
2023-10-28 13:50:12 +0530

    updated ui

--- frontend/src/pages/Stock.jsx
+++ frontend/src/pages/Stock.jsx
@@ -3,7 +3,6 @@ import {
   Box,
   Text,
   Button,
-  SimpleGrid,
   ChakraProvider,
   extendTheme,
   Stat,
@@ -11,7 +10,28 @@ import {
   StatNumber,
   StatArrow,
   HStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
 } from "@chakra-ui/react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 
 const theme = extendTheme({
   styles: {
@@ -27,9 +47,23 @@ const generateRandomDataPoint = (min, max) => {
   return Math.random() * (max - min) + min;
 };
 
+const generateDummyChartData = () => {
+  const dummyData = [];
+  for (let i = 0; i < 30; i++) {
+    dummyData.push({
+      time: `-${30 - i} mins`,
+      price: 150 + generateRandomDataPoint(-15, 15),
+    });
+  }
+  return dummyData.reverse();
+};
+
 const Stock = () => {
-  const [balance, setBalance] = useState(10000);
-  const [stocks, setStocks] = useState([
+  const [balance, setBalance] = useState(() => Number(localStorage.getItem('balance')) || 10000);
+  const [stocks, setStocks] = useState(() => {
+    const storedValue = localStorage.getItem('objectArray');
+    return storedValue ? JSON.parse(storedValue) :
+[
     // your stock data
     { id: "AAPL", name: "Apple Inc.", price: 150.25 },
     { id: "GOOGL", name: "Alphabet Inc.", price: 2765.45 },
@@ -46,20 +80,36 @@ const Stock = () => {
     { id: "V", name: "Visa Inc.", price: 250.80 },
     { id: "GS", name: "The Goldman Sachs Group, Inc.", price: 410.35 },
     { id: "DIS", name: "The Walt Disney Company", price: 175.25 }
-  ]);
+  ];});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedStock, setSelectedStock] = useState(null);
+  const [chartData, setChartData] = useState(generateDummyChartData());
 
   useEffect(() => {
     const interval = setInterval(() => {
       const updatedStocks = stocks.map((stock) => ({
         ...stock,
-        prevPrice: stock.price, // Store previous price
+        prevPrice: stock.price,
         price: stock.price + generateRandomDataPoint(-10, 10),
       }));
       setStocks(updatedStocks);
+
+      const newPrice = stocks[0].price + generateRandomDataPoint(-15, 15);
+      const updatedChartData = [
+        ...chartData.slice(1),
+        { time: "0 mins", price: newPrice },
+      ];
+
+      setChartData(updatedChartData);
     }, 10000);
 
     return () => clearInterval(interval);
-  }, [stocks]);
+  }, [stocks, chartData]);
+
+  const handleModalToggle = (stock) => {
+    setSelectedStock(stock);
+    setIsModalOpen(!isModalOpen);
+  };
 
   const buyStock = (stock) => {
     if (balance >= stock.price) {
@@ -68,29 +118,58 @@ const Stock = () => {
     }
   };
 
+  
+  useEffect(() => {
+    localStorage.setItem('balance', balance.toString());
+  }, [balance]);
+
+  useEffect(() => {
+    localStorage.setItem('stocks', JSON.stringify(stocks));
+  }, [stocks]);
+
+  useEffect(() => {
+    // Code to run when the component mounts (at the start)
+    
+    const intervalId = setInterval(() => {
+      const updatedStocks = stocks.map((stock) => ({
+        ...stock,
+        prevPrice: stock.price, // Store previous price
+        price: stock.price + generateRandomDataPoint(-10, 10),
+      }));
+      setStocks(updatedStocks);
+            console.log('This runs every 1 second.');
+    }, 1000); 
+    
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []); 
+
+
+
   return (
     <ChakraProvider theme={theme}>
       <Box p={6}>
         <Text fontSize="2xl" mb={4}>
           Balance: ${balance.toFixed(2)}
         </Text>
-        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={4}>
-          {stocks.map((stock) => (
-            <Box
-              key={stock.id}
-              p={4}
-              borderWidth="1px"
-              borderRadius="lg"
-              bg="white"
-            >
-              <Text fontSize="xl" mb={2}>
-                {stock.name}
-              </Text>
-              <HStack align="center">
-                <Stat>
-                  <StatLabel>Price</StatLabel>
-                  <StatNumber>${stock.price.toFixed(2)}</StatNumber>
-                </Stat>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Price</Th>
+              <Th>Change</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {stocks.map((stock) => (
+              <Tr key={stock.id}>
+                <Td>{stock.name}</Td>
+                <Td>${stock.price.toFixed(2)}</Td>
+                <Td>
+                   {typeof stock.price === 'number' && typeof stock.prevPrice === 'number' ? (
+
                 <Box
                   p={1}
                   bg={stock.price > stock.prevPrice ? "green.100" : "red.100"}
@@ -106,29 +185,87 @@ const Stock = () => {
                       type={stock.price > stock.prevPrice ? "increase" : "decrease"}
                       color={stock.price > stock.prevPrice ? "green.500" : "red.500"}
                     />
-                    <Box as="span" fontSize="sm" fontWeight="bold">
-                      {(((stock.price - stock.prevPrice) / stock.prevPrice) * 100).toFixed(2)}%
-                    </Box>
+                                           <Box as="span" fontSize="sm" fontWeight="bold">
+
+${(((stock.price - stock.prevPrice) / stock.prevPrice) * 100).toFixed(2)}%
+                                           </Box>
+    
+
+
+
                     </Stat>
                   </HStack>
                 </Box>
-              </HStack>
-              <Button
-                     colorScheme="blue"
-                    _hover={{ bg: "#b6c5fa ", color: "white" }}
-                        bg="blue.500"
-                        color="white"
-                        onClick={() => buyStock(stock)}
->
-                        Buy {stock.name}
-</Button>
-            </Box>
-          ))}
-        </SimpleGrid>
+                  ) : (
+                    <></>
+                  )}
+
+                </Td>
+                <Td>
+                  <Button
+                    colorScheme="blue"
+                    _hover={{ bg: "#b6c5fa", color: "white" }}
+                    bg="blue.500"
+                    color="white"
+                    onClick={() => buyStock(stock)}
+                  >
+                    Buy
+                  </Button>
+                  <Button
+                    colorScheme="teal"
+                    _hover={{ bg: "#b6c5fa", color: "white" }}
+                    bg="teal.500"
+                    color="white"
+                    onClick={() => handleModalToggle(stock)}
+                  >
+                    Stats
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+        {selectedStock && (
+          <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>{selectedStock.name} Statistics</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Text fontSize="xl" mb={2}>
+                  {selectedStock.name}
+                </Text>
+                <AreaChart
+                  width={400}
+                  height={200}
+                  data={chartData}
+                  margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                >
+                  <XAxis dataKey="time" />
+                  <YAxis />
+                  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                  <Tooltip />
+                  <Area
+                    type="monotone"
+                    dataKey="price"
+                    stroke="#8884d8"
+                    fill={(props) =>
+                      props.payload.price > props.payload.prevPrice ? "green" : "red"
+                    }
+                  />
+                </AreaChart>
+              </ModalBody>
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={() => setIsModalOpen(false)}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        )}
       </Box>
     </ChakraProvider>
   );
 };
 
 export default Stock;
-
